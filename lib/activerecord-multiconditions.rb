456c6abcb6c06b34e7fc7bdc772efa1597c425f9
Simone Carletti <weppos@weppos.net>
2009-02-08 16:47:01 +0100

    Added activerecord-multiconditions.rb file to enable RubyGems single-line inclusion.

---
+++ lib/activerecord-multiconditions.rb
@@ -0,0 +1,16 @@
+# 
+# = ActiveRecord::MultiConditions
+#
+# An ActiveRecord plugin for dealing with complex search :conditions.
+# 
+#
+# Category::   ActiveRecord
+# Package::    ActiveRecord::MultiConditions
+# Author::     Simone Carletti <weppos@weppos.net>
+#
+#--
+# SVN: $Id$
+#++
+
+
+require 'multi_conditions'
